dadaadc9674ab3437a45152513fa051dd07ea486
Nikolai Sivertsen <ns@nikolaisivertsen.net>
2019-05-21 15:09:43 +0200

    Work around mobile Safari viewport height problem

--- src/client.js
+++ src/client.js
@@ -1,4 +1,5 @@
 import * as sapper from '@sapper/app';
+import './lib/vh';
 
 sapper.start({
 	target: document.querySelector('#root')
---
+++ src/lib/vh.js
@@ -0,0 +1,13 @@
+import throttle from './throttle';
+
+// Work around Mobile Safari viewport height problem
+// https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+
+const setViewportHeightProperty = () => {
+	const vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+
+setViewportHeightProperty();
+
+window.addEventListener('resize', throttle(setViewportHeightProperty, 30));
