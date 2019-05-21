6c078c2d029cecfc30a978ed6d55cd55827efedd
Nikolai Sivertsen <ns@nikolaisivertsen.net>
2019-05-21 18:26:13 +0200

    Basic error pages

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -5,14 +5,30 @@
 	const dev = process.env.NODE_ENV === 'development';
 </script>
 
+<style>
+	.error {
+		height: 100%;
+		display: flex;
+		flex-flow: column nowrap;
+		justify-content: center;
+		align-items: center;
+		padding: var(--spacing-S) var(--spacing-M);
+	}
+
+	.error h1,
+	.error p {
+		margin-bottom: 1em;
+	}
+</style>
+
 <svelte:head>
 	<title>{status}</title>
 </svelte:head>
 
-<h1>{status}</h1>
-
-<p>{error.message}</p>
-
-{#if dev && error.stack}
-	<pre>{error.stack}</pre>
-{/if}
+<div class='error'>
+	<h1>{status}</h1>
+	<p>{error.message}</p>
+	{#if dev && error.stack}
+		<pre>{error.stack}</pre>
+	{/if}
+</div>
