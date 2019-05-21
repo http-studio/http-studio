b31f504e882cd7652d366c82b2a4518963edb68c
Nikolai Sivertsen <ns@nikolaisivertsen.net>
2019-05-21 12:35:05 +0200

    Add navigation

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,6 +2,32 @@
 	import '../style/base.css';
 </script>
 
+<style>
+	.nav {
+		display: flex;
+		flex-flow: row nowrap;
+		justify-content: space-between;
+		padding: var(--spacing-S) var(--spacing-M);
+	}
+
+	main {
+		flex: 1;
+		overflow-y: scroll;
+		-webkit-overflow-scrolling: touch;
+	}
+</style>
+
+<nav class='nav nav-top'>
+	<a href='/'>Http</a>
+	<a href='/'>Studio</a>
+</nav>
+
 <main>
 	<slot></slot>
 </main>
+
+<nav class='nav nav-bottom'>
+	<a href='mailto:hello@http.studio'>e-mail</a>
+	<a href='https://www.instagram.com/http.studio/'>instagram</a>
+	<a href='https://www.are.na/http-studio'>are.na</a>
+</nav>
